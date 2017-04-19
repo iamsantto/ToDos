ec83ba4656fb303c5b730580e9d6bd08bfc8c163
Santhosh T <santhosh@keepworks.com>
2017-04-19 23:19:44 +0530

    Set up gulp and babel

---
+++ Gulpfile.babel.js
@@ -0,0 +1,13 @@
+import _ from 'lodash'
+import gulp from 'gulp'
+import requireDir from 'require-dir'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const tasks = requireDir('./tasks')
+
+_.forOwn(tasks, (task, name) => {
+  if (name !== 'config')
+    gulp.task(name, task.depends, _.isFunction(task) ? task : undefined)
+})
